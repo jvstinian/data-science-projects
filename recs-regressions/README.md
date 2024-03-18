7a76233c8be376804fe502f3109d5c73f8659a7e
Justin Smith <jvstinian@gmail.com>
2024-03-18 08:47:22 -0400

    Initial commit for RECS regressions.

---
+++ recs-regressions/README.md
@@ -0,0 +1,13 @@
+# RECS Energy Consumption Regressions
+
+This purpose of this notebook provided here is to reproduce the energy consumption regressions described in 
+*The carbon footprint of household energy use in the United States* 
+by Goldstein, Gounaridis, and Newell 
+[PNAS](https://www.pnas.org/doi/abs/10.1073/pnas.1922205117) 
+and more specifically the 
+[Appendix](https://www.pnas.org/doi/suppl/10.1073/pnas.1922205117/suppl_file/pnas.1922205117.sapp.pdf).
+
+# For Nix Users
+A nix-shell environment has been provided. 
+Currently it is pinned to a specific version of the channel `nixos-23.11`.
+
---
+++ recs-regressions/shell.nix
@@ -0,0 +1,22 @@
+# The use of postBuild below does not work as expected.  There's an error about python3 being missing.
+{
+  pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/617579a787259b9a6419492eaac670a5f7663917.tar.gz") {}
+}: 
+let
+  python-packages = ps: with ps; [
+    jupyter
+    ipython
+    pandas
+    numpy
+    statsmodels
+    seaborn
+    tabulate # for writing pandas dataframes to markdown
+  ];
+  python-with-packages = pkgs.python3.withPackages python-packages;
+in 
+pkgs.mkShell {
+  buildInputs = [ python-with-packages ];
+  shellHook = ''
+      jupyter notebook --no-browser --port=8888
+  '';
+}
