d79d1fc06efbd63dfd0be748a64fb9347f04e2a2
Justin Smith <jvstinian@gmail.com>
2024-12-27 18:35:08 -0500

    Cleaning up shell.nix.

--- reinforcement-learning-games/maze/haskell/shell.nix
+++ reinforcement-learning-games/maze/haskell/shell.nix
@@ -1,12 +1,8 @@
 let
   pkgs = import <nixpkgs> {};
-  # config = builtins.readFile /home/justinian/.config/nixpkgs/config.json;
 in
 pkgs.haskellPackages.developPackage {
   root = ./.;
-  modifier = drv: pkgs.haskell.lib.addBuildTools drv ((with pkgs.haskellPackages; [ cabal-install haskell-language-server ]) ++ (with pkgs; [ myNeovim ]));
+  modifier = drv: pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [ cabal-install haskell-language-server ]);
   returnShellEnv = true;
-  # overrides = self: super: {
-  #   inline-c = pkgs.haskell.lib.dontCheck super.inline-c;
-  # };
 }
