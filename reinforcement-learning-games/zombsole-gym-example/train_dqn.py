c2966489962a271ddea93e3ad27597f1ad6aad0e
Justin Smith <jvstinian@gmail.com>
2024-12-28 12:43:43 -0500

    Cleaning up imports and other comments.

--- reinforcement-learning-games/zombsole-gym-example/train_dqn.py
+++ reinforcement-learning-games/zombsole-gym-example/train_dqn.py
@@ -5,31 +5,25 @@ Author: Justin Smith (jvstinian@gmail.com)
 '''
 import os
 import argparse
-# import tensorflow as tf
-# from config import DEMO
-# from task import Task
-# from dpg import DPG
-from dqn.q_learning import DQN
-from dqn.config import DEMO, DEMO_CNN, ZOMBSOLE_MLP
-# from dqn.environment import new_atari_game, new_demo
-import gym
-from zombsole.gym_env import ZombsoleGymEnv, ZombsoleGymEnvDiscreteAction
-from zombsole.renderer import OpencvRenderer
-from gym.envs.registration import registry, register
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
+import gym
+from gym.envs.registration import registry, register
+from zombsole.gym_env import ZombsoleGymEnv, ZombsoleGymEnvDiscreteAction
+from zombsole.renderer import OpencvRenderer
+from dqn.q_learning import DQN
+from dqn.config import DEMO, DEMO_CNN, ZOMBSOLE_MLP
+
 
 register(
     id='Zombsole-v0', 
-    # entry_point='zombsole.gym_env:ZombsoleGymEnv', 
     entry_point='zombsole.gym_env:ZombsoleGymEnvDiscreteAction', 
     max_episode_steps=1000,
     kwargs={
         'rules_name': 'extermination',
-        # 'player_names': ['sniper', 'sniper', 'sniper', 'terminator'],
         'player_names': [],
         'map_name': 'bridge',
-        'agent_id': 0, # 'agent00',
+        'agent_id': 0,
         'initial_zombies': 5,
         'minimum_zombies': 0,
         'debug': False
@@ -52,15 +46,12 @@ def main():
 
     configid = args.config
     if configid == 'zombsole_mlp':
-        # game = gym.make('Zombsole-v0')
-        # pulling this forward from the next branch
         game = gym.make('Zombsole-v0', map_name="easy_exit", rules_name="safehouse", renderer=OpencvRenderer(50, 25))
         conf = ZOMBSOLE_MLP
     else:
         game = gym.make('Zombsole-v0')
         conf = ZOMBSOLE_MLP
 
-    # task = Task(args.task)
     log_dir = os.path.join(conf['log_dir'], '{}/train'.format(args.config))
     if not tf.gfile.Exists(log_dir):
         tf.gfile.MakeDirs(log_dir)
