2f23a3817d77a7462c22ac5dd33531c443013164
Justin Smith <jvstinian@gmail.com>
2025-01-29 09:39:31 -0500

    Removing unnecessary TODO.

--- reinforcement-learning-games/zombsole-gym-example/dqn/q_learning.py
+++ reinforcement-learning-games/zombsole-gym-example/dqn/q_learning.py
@@ -166,7 +166,7 @@ class DQN:
                 if self.verbose:
                     print("episode {}, action {}, total reward {}".format(episode, action, total_reward))
                     print("unscaled state: ", state)
-                    state = self.replay_memory.phi(frame) # TODO: Remove
+                    state = self.replay_memory.phi(frame)
                     print("values for state: ", self.get_tensor_values(sess, state)[2])
 
                 if self.callback:
