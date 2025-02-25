0b6f7374b0d9906a1c7a01b36a7d42bcf570de1e
jvstinian <jvstinian@gmail.com>
2025-02-25 09:14:58 -0500

    Updating DQN config. (#34)

--- reinforcement-learning-games/ma-dqn-train-examples/dqn/config.py
+++ reinforcement-learning-games/ma-dqn-train-examples/dqn/config.py
@@ -4,11 +4,10 @@ author: Justin Smith (jvstinian@gmail.com)
 
 
 ZOMBPYG_MLP = {
-    # 'max_agents': 4, # TODO: Is something like this needed?
     'network_type': 'mlp',
     'gamma': 0.7,
     'batch_size': 1600,
-    'num_episode': 400,
+    'num_episode': 300,
     'capacity': 100000,
     'epsilon_decay': 100000,
     'epsilon_min': 0.1,
@@ -17,9 +16,9 @@ ZOMBPYG_MLP = {
     'time_between_two_copies': 1000,
     'input_scale': 2.0,
     'update_interval': 50,
-    'T': 1490,
+    'T': 6000,
     
-    'learning_rate': 0.5e-2, # tried as low as 0.1e-2 for single-agent
+    'learning_rate': 0.1e-2,
     'optimizer': 'momentum',
     'rho': 0.9, 
     'rmsprop_epsilon': 1e-6,
