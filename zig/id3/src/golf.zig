f460c91705e226b4f3daea5fd282ba475310f54c
Justin Smith <jvstinian@gmail.com>
2025-06-17 23:40:56 -0400

    Starting to fill in golf library.

--- zig/id3/src/golf.zig
+++ zig/id3/src/golf.zig
@@ -1,5 +1,35 @@
 const std = @import("std");
+const id3 = @import("id3.zig");
 const testing = std.testing;
+
+pub const Outlook = enum { sunny, overcast, rain };
+
+pub const WhetherToPlay = enum { dont, do };
+
+pub const Windy = enum(u1) { no = 0, yes = 1 };
+
+pub const HumidityBucket = enum(u1) { le75 = 0, gt75 = 1 };
+
+pub const GolfConditions = struct {
+    id: u8,
+    outlook: Outlook,
+    temperature: u8,
+    humidity: u8,
+    humidity_bucket: HumidityBucket,
+    windy: Windy,
+    play: WhetherToPlay,
+};
+
+fn GolfFieldOffset(comptime field_name: []const u8) comptime_int {
+    return id3.Id3FieldOffset(GolfConditions, field_name);
+}
+
+test "golf field offsets" {
+    std.debug.print("Testing golf field offsets\n", .{});
+    try std.testing.expect(GolfFieldOffset("id") == 0);
+    try std.testing.expect(GolfFieldOffset("outlook") == 1);
+    try std.testing.expect(GolfFieldOffset("windy") == 5);
+}
 
 export fn multiply(a: i32, b: i32) i32 {
     return a * b;
