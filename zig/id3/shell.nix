3559a45411f2e6f0dbb1ddad3de889f6d3929598
Justin Smith <jvstinian@gmail.com>
2025-06-22 19:29:31 -0400

    Adding a nix-shell script for the project.

---
+++ zig/id3/shell.nix
@@ -0,0 +1,13 @@
+# { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {} }:
+# { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz") {} }: # for v0.12.0
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz") {} }: # for v0.13.0
+
+let 
+  myNeovim = (pkgs.config.packageOverrides pkgs).myNeovim;
+in 
+pkgs.mkShell {
+  packages = [ pkgs.zig pkgs.zls myNeovim ];
+  # shellHook = ''
+  #   export DEBUG=1
+  # '';
+}
