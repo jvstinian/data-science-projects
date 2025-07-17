410ed023fe1a491e51953340eba29381055a85c1
jvstinian <jvstinian@gmail.com>
2025-07-17 09:26:55 -0400

    Bumping ID3 to zig 0.14.1 (#73)
    * Bumping to zig 0.14.1.
    * Updating nixpkgs version in flake.

--- zig/id3/shell.nix
+++ zig/id3/shell.nix
@@ -1,7 +1,8 @@
 # { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {} }:
 # { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz") {} }: # for v0.12.0
 # { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz") {} }: # for v0.13.0
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/12a55407652e04dcf2309436eb06fef0d3713ef3.tar.gz") {} }: # for v0.14.0
+# { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/12a55407652e04dcf2309436eb06fef0d3713ef3.tar.gz") {} }: # for v0.14.0
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz") {} }: # for v0.14.1
 
 let 
   myNeovim = (pkgs.config.packageOverrides pkgs).myNeovim;
