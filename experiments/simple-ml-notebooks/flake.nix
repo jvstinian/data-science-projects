ed99f100f8432962cab7edb6aeb41d40931a7322
jvstinian <jvstinian@gmail.com>
2026-03-16 11:11:05 -0400

    Initial look at jupyterenv (#79)
    * Adding a jupyterenv example, which is a flake for JupyterLab that supports multiple languages.
    * Moving python-notebooks to simple-ml-notebooks.

---
+++ experiments/simple-ml-notebooks/flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-23.11";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: 
+      flake-utils.lib.eachDefaultSystem (system:
+        let 
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ ];
+          };
+    
+          dev-python-packages = ps: with ps; [
+              numpy
+              opencv4
+              tensorflowWithCuda
+              keras
+              jupyter
+              ipython
+              pandas
+              numpy
+              statsmodels
+              seaborn
+              plotly
+              tabulate # for writing pandas dataframes to markdown
+              xgboost
+              scikit-learn
+          ];
+          dev-python = pkgs.python3.withPackages dev-python-packages;
+
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            dev-python
+          ];
+          shellHook = "export PS1='\\[\\e[1;34m\\]jupyter > \\[\\e[0m\\]'";
+        };
+      }
+    );
+}
