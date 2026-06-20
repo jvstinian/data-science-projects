950b79b2e3f4193930b81a0fecee351f4a4f605c
jvstinian <jvstinian@gmail.com>
2026-06-20 13:37:08 -0400

    Eligibility Trace algorithms (#96)
    * Adding preliminary eligibility trace algorithms for the frozen lake environment.
    * Adding a TODO in one of the TD algorithms as it looks like there is an error in the action update step.
    * Adding TODOs to address resetting the eligibility trace at the start of an episode.

--- experiments/ada/frozenlake/eligibility_trace_example.adb
+++ experiments/ada/frozenlake/eligibility_trace_example.adb
@@ -5,9 +5,7 @@ with Frozen_Lake.Child;
 with Ada.Numerics.Discrete_Random;
 with Ada.Numerics.Float_Random;
 
-procedure TD_Example is
-    DP_Model : Discrete_Model_Type := Get_Model(Environment_Config'(Map_Name => Map_4x4, Slippery => False));
-    
+procedure Eligibility_Trace_Example is
     package Frozen_Lake_Child is new Frozen_Lake.Child(Map_Info => Get_Map_Info(Map_4x4));
     -- TODO: Use Alt_Discrete_State_Type instead of the following
     type Precise_State_Type is new Integer range 0 .. (Frozen_Lake_Child.Num_Rows * Frozen_Lake_Child.Num_Cols - 1);
@@ -18,16 +16,21 @@ procedure TD_Example is
     type Policy_Type is array (Precise_State_Type) of Action_Type;
 
     type Value_Function_Type is array (Precise_State_Type) of Float;
-    
+
     type Action_Value_Function_Type is array (Precise_State_Type, Action_Type) of Float;
 
-    type TD_Config_Type is record
+    type ET_Config_Type is record
         Alpha : Float;
         Gamma : Float;
+        Lambda : Float;
     end record;
 
-    function TD_Iterative_Policy_Evaluation(Env_Config: Environment_Config; TD_Config: TD_Config_Type; Policy : Policy_Type) return Value_Function_Type is
+    function ET_Iterative_Policy_Evaluation(Env_Config: Environment_Config; ET_Config: ET_Config_Type; Policy : Policy_Type) return Value_Function_Type is
         Env : Environment_State := Make (Env_Config);
+
+        type Eligibility_Trace_Type is array (Precise_State_Type) of Float;
+        E : Eligibility_Trace_Type := (others => 0.0);
+        TD_Error : Float;
 
         Obs : Observation_Type;
         S : Precise_State_Type;
@@ -41,9 +44,6 @@ procedure TD_Example is
         Local_Delta : Float := 0.0;
 
         Prev_Value_Function : Value_Function_Type;
-        -- Prev_Value : Float;
-        -- Transition_Value : Float;
-        -- New_Value : Float;
 
         Episode_Count : Integer := 0;
 
@@ -56,8 +56,9 @@ procedure TD_Example is
             Put_Line("Episode " & Episode_Count'Image);
 
             Prev_Value_Function := Value_Function;
-            Local_Delta := 0.0;
 
+            -- TODO: From the text of the book and the book errata, E needs to be set to 0
+            --       at the beginning of an episode.
             Obs := Reset(Env);
             S := Precise_State_Type(Obs.Position_Index);
             Step_Index := 0;
@@ -69,13 +70,19 @@ procedure TD_Example is
                 Obs := Step_Result.State;
                 S1 := Precise_State_Type(Obs.Position_Index);
                 Put_Line("Action " & Action_Type'Image(Action) & " takes state " & Precise_State_Type'Image(S) & " to state " & Precise_State_Type'Image(S1));
-                Value_Function(S) := Value_Function(S) + TD_Config.Alpha * (Step_Result.Reward + TD_Config.Gamma * Value_Function (S1) - Value_Function (S) );
+                TD_Error := Step_Result.Reward + ET_Config.Gamma * Value_Function (S1) - Value_Function (S);
+                E(S) := E(S) + 1.0;
+                for S2 in Precise_State_Type loop
+                    Value_Function(S2) := Value_Function(S2) + ET_Config.Alpha * TD_Error * E(S2);
+                    E(S2) := ET_Config.Gamma * ET_Config.Lambda * E(S2);
+                end loop;
                 -- Update to next state
                 S := S1;
                 Step_Index := Step_Index + 1;
                 Terminated := Step_Result.Terminated or else Step_Index >= Max_Steps;
             end loop;
 
+            Local_Delta := 0.0;
             -- NOTE: We exit when the max value function change falls below a threshold.
             --       This differs from the textbook algorithm.
             for S in Precise_State_Type loop
@@ -84,17 +91,19 @@ procedure TD_Example is
             exit when Local_Delta < Theta;
         end loop;
         return Value_Function;
-    end TD_Iterative_Policy_Evaluation;
-  
+    end ET_Iterative_Policy_Evaluation;
+
+   -- TODO: Note that there is a type with the same name in the TD example
    type SARSA_Config_Type is record
       Alpha: Float;
       Gamma: Float;
+      Lambda: Float;
       Initial_Epsilon: Float;
       Minimum_Epsilon: Float;
       Episodes_To_Minimum_Epsilon: Natural;
    end record;
 
-   function SARSA_On_Policy(Env_Config: Environment_Config; SARSA_Config: SARSA_Config_Type) return Action_Value_Function_Type is
+   function ET_SARSA_On_Policy(Env_Config: Environment_Config; SARSA_Config: SARSA_Config_Type) return Action_Value_Function_Type is
       Env : Environment_State := Make (Env_Config);
 
       package Action_Unif_Random is new Ada.Numerics.Discrete_Random(Result_Subtype => Action_Type);
@@ -104,6 +113,7 @@ procedure TD_Example is
       Action_Unif_Gen : Action_Unif_Random.Generator;
       Float_Unif_Gen : Float_Unif_Random.Generator;
 
+      -- NOTE: This function is identical to the one in the TD example
       function Best_Action_For_State(Q : Action_Value_Function_Type; S: Precise_State_Type) return Action_Type is
          Max_Value : Float := Float'First;
          Best_Action : Action_Type := Action_Type'First;
@@ -117,6 +127,7 @@ procedure TD_Example is
          return Best_Action;
       end Best_Action_For_State;
 
+      -- NOTE: This function is identical to the one in the TD example
       function Choose_Action_Epsilon_Greedy (Epsilon: Float; Q : Action_Value_Function_Type; S: Precise_State_Type) return Action_Type is
          U : Float := Float_Unif_Random.Random(Float_Unif_Gen);
       begin
@@ -126,6 +137,10 @@ procedure TD_Example is
             return Best_Action_For_State(Q, S);
          end if;
       end Choose_Action_Epsilon_Greedy;
+
+      type Eligibility_Trace_Type is array (Precise_State_Type, Action_Type) of Float;
+      E : Eligibility_Trace_Type := (others => (others => 0.0));
+      TD_Error : Float;
 
       Obs : Observation_Type;
       S : Precise_State_Type;
@@ -137,6 +152,7 @@ procedure TD_Example is
 
       Epsilon : Float;
 
+      -- NOTE: This function is identical to the one in the TD example
       function Update_Epsilon (SARSE_Config: SARSA_Config_Type; Episode : Natural) return Float is
          Init_Eps : Float := SARSA_Config.Initial_Epsilon;
          Min_Eps : Float := SARSA_Config.Minimum_Epsilon;
@@ -170,6 +186,8 @@ procedure TD_Example is
 
             Prev_Action_Value_Function := Action_Value_Function;
 
+            -- TODO: From the text of the book and the book errata, E needs to be set to 0
+            --       at the beginning of an episode.
             Obs := Reset(Env);
             S := Precise_State_Type(Obs.Position_Index);
             A := Choose_Action_Epsilon_Greedy(Epsilon, Action_Value_Function, S);
@@ -182,7 +200,14 @@ procedure TD_Example is
                S1 := Precise_State_Type(Obs.Position_Index);
                A1 := Choose_Action_Epsilon_Greedy(Epsilon, Action_Value_Function, S1);
                Put_Line("Action " & Action_Type'Image(A) & " takes state " & Precise_State_Type'Image(S) & " to state " & Precise_State_Type'Image(S1) & " and action " & Action_Type'Image(A1) & " in on-policy SARSA");
-               Action_Value_Function(S, A) := Action_Value_Function(S, A) + SARSA_Config.Alpha * (Step_Result.Reward + SARSA_Config.Gamma * Action_Value_Function (S1, A1) - Action_Value_Function (S, A) );
+               TD_Error := Step_Result.Reward + SARSA_Config.Gamma * Action_Value_Function (S1, A1) - Action_Value_Function (S, A);
+               E(S, A) := E(S, A) + 1.0;
+               for S2 in Precise_State_Type loop
+                  for A2 in Action_Type loop
+                      Action_Value_Function(S2, A2) := Action_Value_Function(S2, A2) + SARSA_Config.Alpha * TD_Error * E(S2, A2);
+                      E(S2, A2) := SARSA_Config.Gamma * SARSA_Config.Lambda * E(S2, A2);
+                  end loop;
+               end loop;
                 -- Update to next state
                S := S1;
                A := A1;
@@ -203,9 +228,9 @@ procedure TD_Example is
             exit when Episode_Count >= SARSA_Config.Episodes_To_Minimum_Epsilon and then Local_Delta < Theta;
         end loop;
         return Action_Value_Function;
-    end SARSA_On_Policy;
-   
-    function SARSA_Off_Policy(Env_Config: Environment_Config; SARSA_Config: SARSA_Config_Type) return Action_Value_Function_Type is
+    end ET_SARSA_On_Policy;
+
+   function Watkins_Q_Iteration(Env_Config: Environment_Config; SARSA_Config: SARSA_Config_Type) return Action_Value_Function_Type is
       Env : Environment_State := Make (Env_Config);
 
       package Action_Unif_Random is new Ada.Numerics.Discrete_Random(Result_Subtype => Action_Type);
@@ -215,6 +240,7 @@ procedure TD_Example is
       Action_Unif_Gen : Action_Unif_Random.Generator;
       Float_Unif_Gen : Float_Unif_Random.Generator;
 
+      -- NOTE: This function is identical to the one in the TD example
       function Best_Action_For_State(Q : Action_Value_Function_Type; S: Precise_State_Type) return Action_Type is
          Max_Value : Float := Float'First;
          Best_Action : Action_Type := Action_Type'First;
@@ -228,6 +254,7 @@ procedure TD_Example is
          return Best_Action;
       end Best_Action_For_State;
 
+      -- NOTE: This function is identical to the one in the TD example
       function Choose_Action_Epsilon_Greedy (Epsilon: Float; Q : Action_Value_Function_Type; S: Precise_State_Type) return Action_Type is
          U : Float := Float_Unif_Random.Random(Float_Unif_Gen);
       begin
@@ -238,16 +265,22 @@ procedure TD_Example is
          end if;
       end Choose_Action_Epsilon_Greedy;
 
+      type Eligibility_Trace_Type is array (Precise_State_Type, Action_Type) of Float;
+      E : Eligibility_Trace_Type := (others => (others => 0.0));
+      TD_Error : Float;
+
       Obs : Observation_Type;
       S : Precise_State_Type;
       S1 : Precise_State_Type;
       Step_Result : Step_Return_Type;
       A : Action_Type;
       A1 : Action_Type;
+      A_Best : Action_Type;
       Terminated : Boolean := False;
 
       Epsilon : Float;
 
+      -- NOTE: This function is identical to the one in the TD example
       function Update_Epsilon (SARSE_Config: SARSA_Config_Type; Episode : Natural) return Float is
          Init_Eps : Float := SARSA_Config.Initial_Epsilon;
          Min_Eps : Float := SARSA_Config.Minimum_Epsilon;
@@ -281,22 +314,37 @@ procedure TD_Example is
 
             Prev_Action_Value_Function := Action_Value_Function;
 
+            -- TODO: From the text of the book and the book errata, E needs to be set to 0
+            --       at the beginning of an episode.
             Obs := Reset(Env);
             S := Precise_State_Type(Obs.Position_Index);
+            A := Choose_Action_Epsilon_Greedy(Epsilon, Action_Value_Function, S);
             Step_Index := 0;
             Terminated := False;
 
             while not Terminated loop
-                -- TODO: I think there's an error here.  A needs to be moved up (out of the loop)
-               A := Choose_Action_Epsilon_Greedy(Epsilon, Action_Value_Function, S);
                Step_Result := Step(Env, A);
                Obs := Step_Result.State;
                S1 := Precise_State_Type(Obs.Position_Index);
-               A1 := Best_Action_For_State(Action_Value_Function, S1);  -- TODO: Just get best value for state
-               Put_Line("Action " & Action_Type'Image(A) & " takes state " & Precise_State_Type'Image(S) & " to state " & Precise_State_Type'Image(S1) & " in off-policy SARSA");
-               Action_Value_Function(S, A) := Action_Value_Function(S, A) + SARSA_Config.Alpha * (Step_Result.Reward + SARSA_Config.Gamma * Action_Value_Function (S1, A1) - Action_Value_Function (S, A) );
+               A1 := Choose_Action_Epsilon_Greedy(Epsilon, Action_Value_Function, S1);
+               A_Best := Best_Action_For_State(Action_Value_Function, S1);  -- TODO: Just get best value for state
+               Put_Line("Action " & Action_Type'Image(A) & " takes state " & Precise_State_Type'Image(S) & " to state " & Precise_State_Type'Image(S1) & " in Watkin's Q algorithm (off-policy SARSA usingn Eligibility Trace)");
+               TD_Error := Step_Result.Reward + SARSA_Config.Gamma * Action_Value_Function (S1, A_Best) - Action_Value_Function (S, A);
+               E(S, A) := E(S, A) + 1.0;
+               for S2 in Precise_State_Type loop
+                  for A2 in Action_Type loop
+                      Action_Value_Function(S2, A2) := Action_Value_Function(S2, A2) + SARSA_Config.Alpha * TD_Error * E(S2, A2);
+                      -- IN PROGRESS: Is the following correct?  Based on the text, I think so.
+                      if A1 = A_Best then
+                         E(S2, A2) := SARSA_Config.Gamma * SARSA_Config.Lambda * E(S2, A2);
+                      else 
+                         E(S2, A2) := 0.0;
+                      end if;
+                  end loop;
+               end loop;
                 -- Update to next state
                S := S1;
+               A := A1;
                Step_Index := Step_Index + 1;
                Terminated := Step_Result.Terminated or else Step_Index >= Max_Steps;
             end loop;
@@ -314,27 +362,28 @@ procedure TD_Example is
             exit when Episode_Count >= SARSA_Config.Episodes_To_Minimum_Epsilon and then Local_Delta < Theta;
         end loop;
         return Action_Value_Function;
-    end SARSA_Off_Policy;
+    end Watkins_Q_Iteration;
 
     Frozen_Lake_Config : Environment_Config := (Map_Name => Map_4x4, Slippery => False);
-    TD_Config : TD_Config_Type := (Alpha => 0.1, Gamma => 0.9);
+    ET_Config : ET_Config_Type := (Alpha => 0.1, Gamma => 0.9, Lambda => 0.6);
     P : Policy_Type := (others => Down);
     Local_Value_Function : Value_Function_Type;
 
     -- SARSA
     SARSA_Config : SARSA_Config_Type := (
       Alpha => 0.1, Gamma => 0.9,
+      Lambda => 0.6,
       Initial_Epsilon => 0.8, Minimum_Epsilon => 0.01,
       Episodes_To_Minimum_Epsilon => 50000
     );
     Local_Action_Value_Function : Action_Value_Function_Type;
 begin
-    Put_Line("TD iterative policy evaluation for Frozen Lake environment");
+    Put_Line("Eligibility Trace iterative policy evaluation for Frozen Lake environment");
     P (8) := Right;
     P (13) := Right;
     P (14) := Right;
-    
-    Local_Value_Function := TD_Iterative_Policy_Evaluation(Frozen_Lake_Config, TD_Config, P);
+
+    Local_Value_Function := ET_Iterative_Policy_Evaluation(Frozen_Lake_Config, ET_Config, P);
     for S in Precise_State_Type loop
         -- (S, F, F, F),
         -- (F, H, F, H),
@@ -345,8 +394,8 @@ begin
         New_Line;
     end loop;
 
-    Put_Line("Running on-policy SARSA");
-    Local_Action_Value_Function := SARSA_On_Policy(Frozen_Lake_Config, SARSA_Config);
+    Put_Line("Running on-policy Eligibility Trace SARSA");
+    Local_Action_Value_Function := ET_SARSA_On_Policy(Frozen_Lake_Config, SARSA_Config);
     for S in Precise_State_Type loop
        for A in Action_Type loop
          Put("Value of (" & S'Image & ", " & Action_Type'Image(A) & "): ");
@@ -354,9 +403,9 @@ begin
          New_Line;
        end loop;
     end loop;
-    
-    Put_Line("Running off-policy SARSA");
-    Local_Action_Value_Function := SARSA_Off_Policy(Frozen_Lake_Config, SARSA_Config);
+
+    Put_Line("Running Watkins Q Control Method");
+    Local_Action_Value_Function := Watkins_Q_Iteration(Frozen_Lake_Config, SARSA_Config);
     for S in Precise_State_Type loop
        for A in Action_Type loop
          Put("Value of (" & S'Image & ", " & Action_Type'Image(A) & "): ");
@@ -364,5 +413,5 @@ begin
          New_Line;
        end loop;
     end loop;
-end TD_Example;
-    
+end Eligibility_Trace_Example;
+
