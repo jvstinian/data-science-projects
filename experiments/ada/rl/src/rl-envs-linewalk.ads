f0dca93f9d8013f48ba784d2f8b9fe9f795a8cc0
Justin Smith <jvstinian@gmail.com>
2026-06-25 15:43:53 -0400

    Addressing linewalk todos.

--- experiments/ada/rl/src/rl-envs-linewalk.ads
+++ experiments/ada/rl/src/rl-envs-linewalk.ads
@@ -9,15 +9,13 @@ package RL.Envs.LineWalk is
     type Available_Actions_Type is array (Action_Type) of Boolean;
     type Valid_Actions_Type is array (Natural range <>) of Action_Type;
 
-    -- TODO: Remove discriminant on public declaration?
-    -- type State_Type is private;
-    type State_Type(Kind : State_Kind_Type := Active) is private;
+    type State_Type is private;
 
     function Initial_State return State_Type;
     function Is_Terminal (State : State_Type) return Boolean;
     function Get_Player(State : State_Type) return Player_Type;
     function Step(State : State_Type; Action : Action_Type) return State_Type;
-    function Reward(Player: Player_Type; State : State_Type) return Long_Float;
+    function Reward(Player: Player_Type; State : State_Type) return Float;
     function Get_Available_Actions (State : State_Type) return Available_Actions_Type;
     function Get_Valid_Actions (State : State_Type) return Valid_Actions_Type;
 
