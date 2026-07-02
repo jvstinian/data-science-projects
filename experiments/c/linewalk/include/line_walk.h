198eec34fd44c5a1cd5fd8c9d7f71503104f9db1
jvstinian <jvstinian@gmail.com>
2026-07-02 10:19:38 -0400

    Add RL interface to C linewalk environment (#131)
    * Adding the RL interface to the linewalk environment.  Adding a new X-macro for simulating an episode with uniformly chosen random actions.
    * RL C Linewalk updates (#132)
    * Updating linewalk-c.  Removing the original methods that the templates were intended to replace.  Adding some additional macro checks.
    * Removing a declaration of a function that was deleted.

--- experiments/c/linewalk/include/line_walk.h
+++ experiments/c/linewalk/include/line_walk.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 typedef struct LineWalkConfig {
     int N; /* Number of positions in the line */
 } LineWalkConfig;
@@ -28,15 +30,48 @@ typedef struct LineWalkState {
     int reward;
 } LineWalkState;
 
+/* MCTS Interface */
 LineWalkState initial_state(LineWalkConfig config);
 Boolean is_terminal (LineWalkState state);
 Player get_player(LineWalkState state);
 LineWalkState step(LineWalkState state, Action action);
 float reward(Player player, LineWalkState state);
 unsigned int get_available_actions (LineWalkState state, Action *available_actions, unsigned int* num_actions);
+enum Action linewalk_mctsenv_get_random_action(LineWalkState state);
 void print_state(LineWalkState state);
 
-void take_random_action(const LineWalkConfig* config, unsigned int max_steps);
+/* RL Interface */
+struct LineWalkObservation {
+    unsigned short int position;
+};
+
+/* LineWalkEnvironment declaration */
+struct LineWalkEnvironment;
+
+struct LineWalkStepReturn {
+    struct LineWalkObservation observation;
+    float reward;
+    Boolean terminated;
+};
+ 
+struct LineWalkEnvironment* linewalk_make(struct LineWalkConfig config);
+int linewalk_init(struct LineWalkConfig config, struct LineWalkEnvironment* env);
+/* TODO: Add support for a seed reset */
+struct LineWalkObservation linewalk_reset(struct LineWalkEnvironment* env);
+struct LineWalkStepReturn linewalk_step(struct LineWalkEnvironment* env, enum Action action);
+void linewalk_deinit(struct LineWalkEnvironment* env);
+void linewalk_close(struct LineWalkEnvironment* env);
+
+void linewalk_mctsenv_uniform_random_actions(const LineWalkConfig* config, unsigned int max_steps);
+
+
+struct SimulationSummary {
+    size_t num_steps;
+    float total_reward;
+};
+
+enum Action linewalk_get_random_action(struct LineWalkEnvironment* env);
+struct SimulationSummary linewalk_uniform_random_actions(struct LineWalkConfig config, Boolean verbose);
 
 #define CONFIG_TYPE LineWalkConfig
 #include "random_action_h.inc"
--- experiments/c/linewalk/src/line_walk.c
+++ experiments/c/linewalk/src/line_walk.c
@@ -67,6 +67,11 @@ unsigned int get_available_actions (LineWalkState state, Action *available_actio
     return count;
 }
 
+enum Action linewalk_mctsenv_get_random_action(LineWalkState state) {
+    (void)state;
+    return (enum Action) rand() % 2;
+}
+
 void print_state(LineWalkState state) {
     if (state.kind == ACTIVE) {
         printf("Current position: %u\n", state.position);
@@ -76,41 +81,125 @@ void print_state(LineWalkState state) {
 }
 
 
+struct LineWalkEnvironment {
+    LineWalkConfig config;
+    unsigned short int position;
+};
+
+struct LineWalkEnvironment* linewalk_make(struct LineWalkConfig config) {
+    struct LineWalkEnvironment* env = malloc(sizeof(struct LineWalkEnvironment));
+    if (env == NULL) {
+        fprintf(stderr, "linewalk: Failed to allocate memory for LineWalkEnvironment\n");
+        return NULL;
+    }
+    if (linewalk_init(config, env)) {
+        fprintf(stderr, "linewalk: Failed to initialize LineWalkEnvironment\n");
+        free(env);
+        return NULL;
+    }
+    return env;
+}
+
+int linewalk_init(struct LineWalkConfig config, struct LineWalkEnvironment* env) {
+    env->config = config;
+    env->position = (config.N + 1) / 2; /* Start at the middle position */
+    return 0;
+};
+
+struct LineWalkObservation linewalk_reset(struct LineWalkEnvironment* env) {
+    env->position = (env->config.N + 1) / 2; /* Reset to middle position */
+    return (struct LineWalkObservation) { env->position };
+}
+
+struct LineWalkStepReturn linewalk_step(struct LineWalkEnvironment* env, enum Action action) {
+    /* We reinvent the wheel rather than using the step method
+     * that was defined above */
+    unsigned short int new_pos = env->position;
+    float reward;
+    Boolean terminated;
+    struct LineWalkStepReturn ret;
+
+    if (new_pos > 0 && new_pos < (env->config.N + 1)) {
+        switch (action) {
+            case MOVE_LEFT:
+                new_pos -= 1;
+                break;
+            case MOVE_RIGHT:
+                new_pos += 1;
+                break;
+        }
+    }
+    if (new_pos < 1) {
+        terminated = TRUE;
+        reward = -1.0f;
+    } else if (new_pos > env->config.N) {
+        terminated = TRUE;
+        reward = 1.0f;
+    } else {
+        terminated = FALSE;
+        reward = 0.0f;
+    }
+
+    env->position = new_pos;
+    ret.observation.position = new_pos;
+    ret.reward = reward;
+    ret.terminated = terminated;
+
+    return ret;
+}
+
+void linewalk_deinit(struct LineWalkEnvironment* env) {
+    (void)env;
+    /* Nothing to do */
+}
+
+void linewalk_close(struct LineWalkEnvironment* env) {
+    linewalk_deinit(env);
+    free(env);
+}
+
+
+#define ENVIRONMENT_PREFIX linewalk
 #define CONFIG_TYPE LineWalkConfig
 #define STATE_TYPE LineWalkState
 #define ACTION_TYPE enum Action
 #define STEP_METHOD step
-#include "random_action_c.inc"
+#define RANDOM_ACTION_METHOD linewalk_mctsenv_get_random_action
+#define IS_TERMINAL_METHOD is_terminal
+#include "mctsenv_uniform_random_actions_c.inc"
+#undef IS_TERMINAL_METHOD
+#undef RANDOM_ACTION_METHOD 
 #undef STEP_METHOD
 #undef ACTION_TYPE
 #undef STATE_TYPE
 #undef CONFIG_TYPE
+#undef ENVIRONMENT_PREFIX
 
-void take_random_action(const LineWalkConfig* config, unsigned int max_steps) {
-    LineWalkState state = initial_state(*config);
-    size_t i;
-    enum Action action;
-    Boolean terminated = FALSE;
-
-    srand(123);
-    
-    for(i = 0; i < max_steps; i++) {
-        action = (enum Action) rand() % 2;
-        if (action == MOVE_LEFT) {
-            printf("Moving left\n");
-        } else if (action == MOVE_RIGHT) {
-            printf("Moving right\n");
-        }
-        state = step(state, action);
-        if ((terminated = is_terminal(state))) {
-            break;
-        }
-    }
-
-    if (terminated) {
-        printf("Terminated\n");
-    } else {
-        printf("Environment stopped after %u steps\n", max_steps);
-    }
-    printf("Reward: %f", reward(PLAYER1, state));
+enum Action linewalk_get_random_action(struct LineWalkEnvironment* env) {
+    (void)env;
+    return (enum Action) rand() % 2;
 }
+
+#define ENVIRONMENT_PREFIX linewalk
+#define CONFIG_TYPE struct LineWalkConfig
+#define ENVIRONMENT_TYPE struct LineWalkEnvironment
+#define OBSERVATION_TYPE struct LineWalkObservation
+#define STEPRETURN_TYPE struct LineWalkStepReturn
+#define ACTION_TYPE enum Action
+#define MAKE_METHOD linewalk_make
+#define RESET_METHOD linewalk_reset
+#define RANDOM_ACTION_METHOD linewalk_get_random_action
+#define STEP_METHOD linewalk_step
+#define CLOSE_METHOD linewalk_close
+#include "uniform_random_actions_c.inc"
+#undef CLOSE_METHOD
+#undef STEP_METHOD
+#undef RANDOM_ACTION_METHOD
+#undef RESET_METHOD
+#undef MAKE_METHOD
+#undef ACTION_TYPE
+#undef STEPRETURN_TYPE
+#undef OBSERVATION_TYPE
+#undef ENVIRONMENT_TYPE
+#undef CONFIG_TYPE
+#undef ENVIRONMENT_PREFIX
