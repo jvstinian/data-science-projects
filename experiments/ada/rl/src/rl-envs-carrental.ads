d19cffd6310efdadee93c6b8a237ea3ee856cbad
jvstinian <jvstinian@gmail.com>
2026-07-07 10:56:38 -0400

    RL Ada car rental environment improvements (#140)
    * Changing names of Carrental types to match other environments.  Adding a description of the environment.
    * Capitalizing function input.
    * RL Ada additional car rental environment improvements  (#141)
    * Updating logic for setting the seed in the car rental environment.  Removing the old RNG state from the package.  Removing unused methods.
    * Updating description.
    * Removing some unnecessary code.
    * RL Ada car rental test case (#142)
    * Adding a simple car rental environment test.
    * Cleaning up and reorganizing code.
    * Adding test cases for the car rental environment.  Bounding the Poisson CDF at 1 to avoid negative transition probabilities. (#143)

--- experiments/ada/rl/src/rl-envs-carrental.ads
+++ experiments/ada/rl/src/rl-envs-carrental.ads
@@ -1,8 +1,82 @@
--- with Ada.Numerics;
 with Ada.Numerics.Float_Random;
 with Mathpaqs.Generic_Random_Functions;
 with System.Pool_Local;
 
+--  ## Description
+--  The Car Rental environment involves an owner of a car rental business
+--  which has two locations, which are referred to as lot A and B.  Each
+--  day customers come in to rent and return cars.  A car that is returned
+--  is only available for rental the next day.  At the end of each
+--  day the owner must make a choice about how many cars to move between
+--  the two sites with the aim being to make sure there
+--  are enough cars available for rental at each site.
+--  The owner makes a fixed profit for every car rented, and
+--  pays a fixed amount for each car moved overnight.
+--
+--  The number of rental requests and returns at each site follow
+--  independent Poisson processes.  The lot size at each location
+--  is fixed and so there is a maximum number of cars that can
+--  be made available for rental at each location.  If the number
+--  of returns would result in an overflow at the lot then the
+--  excess cars are returned to a lot maintained by the corporate
+--  headquarters and are not subsequently available for rent.
+--  
+--  The number of cars that can be moved overnight is fixed as well.
+--
+--  The parameters and constraints referenced above are mostly
+--  fixed (constant) in this implementation, i.e. we don't
+--  support specifying these parameters either as generic
+--  parameters or as values in the configuration type.
+--  We will provide further information about the values of
+--  these constants in the sections that follow.
+--  The Poisson mean request and returns rates and the initial
+--  number of cars in each lot can be specified though.
+-- 
+--  This environment is adapted from Example 4.2 (page 98) of
+--  Reinforcement Learning: An Introduction (1998) by Sutton and Barto.
+--
+--  ## Action Space
+--  Actions represent the number of cars transferred between Lot A
+--  and Lot B.  The maximum number of cars that can be moved
+--  is 5.  The range for the action values is -5, -4, ..., +5.
+--  A positive number indicates cars were moved from Lot A to B,
+--  while a negative number indicates cars were moved in the
+--  opposite direction.
+-- 
+--  ## Observation Space
+--  The observation space consists of two values, namely the number of
+--  cars present on each lot at the end of the day.  These numbers
+--  reflect the cars rented and returned that day.
+--  For each lot, the number of cars on each lot is an integer
+--  between 0 and 20 (inclusive).  If the returns would
+--  cause this number to be above 20, then those cars are assumed
+--  to be moved to a lot maintained by corporate headquarters
+--  and are not available for rent.
+--
+--  For the Dynamic Programming model, we require a discrete state,
+--  and so we convert the number of cars in the two lots to a
+--  single value.  The approach used is the same approach used to
+--  convert coordinates in other environments to a single
+--  position index.  If the number of cars is given by a tuple
+--  (a, b), then the corresponding discrete state value is given
+--  by a * 21 + b.
+--
+--  ## Rewards
+--  The owner makes a profit of $10 dollars for each car rented,
+--  and pays $2 for each car moved.
+--  Only cars available at the lot can be rented, and
+--  so no profit is made for any requests in excess of the
+--  number of cars available (the customers are turned away).
+--
+--  ## Environment Configuration
+--  The Poisson mean rate for returns and requests
+--  at each lot can be specified.  Also, the number of
+--  cars initially available at each site can be provided.
+--
+--  ## References
+--  Sutton, R. S., Barto, A. G. (2018).
+--  Reinforcement Learning: An Introduction. The MIT Press.
+--  http://incompleteideas.net/book/the-book-1st.html
 package RL.Envs.Carrental is
    package Float_Random renames Ada.Numerics.Float_Random;
    package GRF is new Mathpaqs.Generic_Random_Functions(Real => Float);
@@ -12,7 +86,7 @@ package RL.Envs.Carrental is
 
    type Action_Type is new Integer range -Max_Move .. Max_Move;
 
-   type Environment_Config is record
+   type Config_Type is record
       Lot_A_Request_Lambda : Float;
       Lot_A_Return_Lambda : Float;
       Lot_B_Request_Lambda : Float;
@@ -21,7 +95,7 @@ package RL.Envs.Carrental is
       Lot_B_Init_Cars : Natural;
    end record;
 
-   Default_Config : Environment_Config := (
+   Default_Config : Config_Type := (
       Lot_A_Request_Lambda => 3.0,
       Lot_A_Return_Lambda => 3.0,
       Lot_B_Request_Lambda => 4.0,
@@ -30,7 +104,7 @@ package RL.Envs.Carrental is
       Lot_B_Init_Cars => 0
    );
 
-   type Environment_State is limited private;
+   type Environment_Type is limited private;
 
    type Observation_Type is record
       Lot_A_Cars : Natural;
@@ -41,69 +115,34 @@ package RL.Envs.Carrental is
       Observation: Observation_Type;
       Reward: Float;
       Terminated: Boolean;
-      Truncated: Boolean;
    end record;
     
-   function Make(config: Environment_Config) return Environment_State;
-   -- TODO: Add argument for seed
-   function Reset(Env : in out Environment_State) return Observation_Type;
-   function Step(Env : in out Environment_State; action: Action_Type) return Step_Return_Type;
-   procedure Render_Text(Env : Environment_State);
+   function Make(Config: Config_Type) return Environment_Type;
+   function Reset(Env : in out Environment_Type; Seed_Reset : Seed_Reset_Type) return Observation_Type;
+   function Step(Env : in out Environment_Type; Action: Action_Type) return Step_Return_Type;
+   procedure Render_Text(Env : Environment_Type);
 
    type Discrete_State_Type is new Natural range 0 .. ((Lot_Size + 1) * (Lot_Size + 1) - 1);
-   type Transition_Probability_Type is record
-       Probability : Float;
-       Reward : Float;
-   end record;
-   type Discrete_Model_Type is array (Discrete_State_Type, Action_Type, Discrete_State_Type) of Transition_Probability_Type;
-   type Discrete_Model_Access_Type is access Discrete_Model_Type;
+   type DP_Model_Type is array (Discrete_State_Type, Action_Type, Discrete_State_Type) of Transition_Probability_Type;
+   type DP_Model_Access_Type is access DP_Model_Type;
    DM_Pool : System.Pool_Local.Unbounded_Reclaim_Pool;
-   for Discrete_Model_Access_Type'Storage_Pool use DM_Pool;
-   function Get_Model(Config: Environment_Config) return Discrete_Model_Access_Type;
-   -- function Get_Model_Access(Config: Environment_Config) return Discrete_Model_Access_Type;
+   for DP_Model_Access_Type'Storage_Pool use DM_Pool;
+   function Get_Model(Config: Config_Type) return DP_Model_Access_Type;
 
    type Transition_Array_Type is array (Discrete_State_Type) of Transition_Probability_Type;
-   function Get_Transition_Values (Config: Environment_Config; State: Discrete_State_Type; Action: Action_Type) return Transition_Array_Type;
-   function Get_Transition_Values2 (Config: Environment_Config; State: Discrete_State_Type; Action: Action_Type) return Transition_Array_Type;
-
+   function Collect_Transition_Values (Config: Config_Type; State: Discrete_State_Type; Action: Action_Type) return Transition_Array_Type;
+   function Get_Transition_Values_From_State (Config: Config_Type; State: Discrete_State_Type; Action: Action_Type) return Transition_Array_Type;
 private
-   -- -- The following is a partial set of fields for the
-   -- -- Transition_Type which follows
-   -- type Partial_Transition_Type is record
-   --     Position : Position_Type;
-   --     Reward : Float;
-   --     Truncated : Boolean; -- TODO: Change variable name to Terminated
-   -- end record;
-
-   -- type Transition_Type is record
-   --     Probability : Float;
-   --     Position : Position_Type;
-   --     Reward : Float;
-   --     Truncated : Boolean; -- TODO: Change variable name to Terminated
-   -- end record;
-
-   -- -- type Transition_Probabilities_Type is array (Natural range <>) of Transition_Type;
-   -- type Action_Transition_Type is array (Action_Type, Action_Type) of Transition_Type;
-
-   -- type Map_Transitions is array (Positive range <>, Positive range <>) of Action_Transition_Type;
-
-   Gen: Float_Random.Generator;
-   function Rando return Float;
-   function Poisson is new GRF.Poisson(U => Rando);
-
-   type Environment_State is record
+   type Environment_Type is record
       Gen: Float_Random.Generator;
-      Config : Environment_Config;
-      -- Lot_A_Request_Lambda : Float;
-      -- Lot_A_Return_Lambda : Float;
-      -- Lot_B_Request_Lambda : Float;
-      -- Lot_B_Return_Lambda : Float;
-      -- Lot_A_Init_Cars : Natural;
-      -- Lot_B_Init_Cars : Natural;
+      Config : Config_Type;
       Lot_A_Cars : Natural;
       Lot_B_Cars : Natural;
    end record;
-      
+
+   -- Cars_Per_Lot_Type is the same as Observation_Type, but
+   -- it is kept distinct as it is used in private methods for
+   -- calculating transition probabilities.
    type Cars_Per_Lot_Type is record
       Lot_A_Cars : Natural;
       Lot_B_Cars : Natural;
@@ -117,17 +156,25 @@ private
    function Poisson_PMF(Lambda : Float; N : Natural) return Float;
    function Poisson_CDF(Lambda : Float; N : Natural) return Float;
    function Poisson_SF(Lambda : Float; N : Natural) return Float;  -- Survival function, i.e., 1 - CDF
-   -- -- These functions follow similar methods in the Python implementation of FrozenLakeEnv.
-   -- -- We make these private since they are not intended to be used directly.
-   -- function Position_Inc(Rows : Positive; Cols : Positive; Position: Position_Type; Action: Action_Type) return Position_Type;
-   -- function Update_Probability_Matrix(Map : Map_Array; Position: Position_Type; Action : Action_Type) return Partial_Transition_Type;
-   function To_Obs(Env : Environment_State) return Natural;
-   function To_Discrete_State(Cars_Per_Lot : Cars_Per_Lot_Type) return Discrete_State_Type;
-   -- function Get_Start_Position(Map: Map_Array) return Position_Type;
-   function From_Discrete_State (D : Discrete_State_Type) return Cars_Per_Lot_Type;
-   function Calculate_Transition_Probability (Config : Environment_Config; Cars_Moved : Natural; Prev_Cars : Cars_Per_Lot_Type; Next_Cars : Cars_Per_Lot_Type) return Transition_Probability_Type;
-   function Step_Cars(Cars_Count : Cars_Per_Lot_Type; Action : Action_Type) return Cars_After_Action_Type;
-   function Calculate_Transition_Probability2 (Config : Environment_Config; Cars_Moved : Natural; Prev_Cars : Cars_Per_Lot_Type) return Transition_Array_Type;
 
+   -- The following convert the observation type to and from the
+   -- discrete representation.
+   function To_Discrete_State(Cars_Per_Lot : Cars_Per_Lot_Type) return Discrete_State_Type;
+   function From_Discrete_State (D : Discrete_State_Type) return Cars_Per_Lot_Type;
+
+   -- The following are for calculating the transition probabilities for the
+   -- dynamic programming model.
+   function Step_Cars(Cars_Count : Cars_Per_Lot_Type; Action : Action_Type) return Cars_After_Action_Type;
+   -- There are two methods for calculating transition probabilities, though only one is
+   -- currently used in the discrete model calculations.
+   -- The first method calculates the transition probability between two specific states,
+   -- the "initial" state actually being the state immediately after the action has been applied
+   -- (or more specifically after the cars have been moved between sites overnight
+   -- but before the rental requests and returns that occur the next day).
+   function Calculate_Transition_Probability_Between_States (Config : Config_Type; Cars_Moved : Natural; Prev_Cars : Cars_Per_Lot_Type; Next_Cars : Cars_Per_Lot_Type) return Transition_Probability_Type;
+   -- The second method calculates all the transition probabilities starting from the
+   -- post-action state.  The output is an array of transition probabilities for
+   -- all possible next states.
+   function Calculate_Transition_Probabilities_From_State (Config : Config_Type; Cars_Moved : Natural; Prev_Cars : Cars_Per_Lot_Type) return Transition_Array_Type;
 end RL.Envs.Carrental;
 
