5cce7181f8055f42d8852c9e3b7abbc64ebe961d
Justin Smith <jvstinian@gmail.com>
2026-07-17 17:56:22 -0400

    Refactoring the policy iteration algorithm into two methods.  One takes initial state values and an initial policy, and the other uses a default state value function and policy.

--- experiments/ada/rl/src/rl-algorithms-dp.adb
+++ experiments/ada/rl/src/rl-algorithms-dp.adb
@@ -76,58 +76,11 @@ package body RL.Algorithms.DP is
       end loop;
    end Print_Policy;
 
-    -- function Policy_Iteration(Model : DP_Model_Type; Discount_Factor : Float) return Deterministic_Policy_Type is
-    --     type Action_Value_Array_Type is array (Action_Type) of Float;
-
-    --     function Arg_Max(Action_Values : Action_Value_Array_Type) return Action_Type is
-    --         Max_Value : Float := Float'First;
-    --         Best_Action : Action_Type := Action_Type'First;
-    --     begin
-    --         for A in Action_Type loop
-    --             if Action_Values(A) > Max_Value then
-    --                 Max_Value := Action_Values(A);
-    --                 Best_Action := A;
-    --             end if;
-    --         end loop;
-    --         return Best_Action;
-    --     end Arg_Max;
-
-    --     Value_Function : Value_Function_Type := (others => 0.0);  -- TODO: Perhaps initialize to random values?
-    --     Policy : Deterministic_Policy_Type := (others => Action_Random.Random(Action_Gen));  -- Initialize to random policy
-
-    --     Stable : Boolean;
-    --     Prev_Action : Action_Type;
-    --     Action_Values : Action_Value_Array_Type;
-    -- begin
-    --     Print_Policy(Policy);  -- TODO: Remove after debugging
-
-    --     loop 
-    --         Value_Function := Iterative_Policy_Evaluation(Model, Policy, Discount_Factor, Value_Function);
-    -- 
-    --         Stable := True;
-    --         for S in State_Type loop
-    --             -- For state S, record the value of each action in Action_Values.
-    --             for A in Action_Type loop
-    --                 Action_Values(A) := 0.0;
-    --                 for S1 in State_Type loop
-    --                     Action_Values(A) := Action_Values(A) + Model(S, A, S1).Probability * (Model(S, A, S1).Reward + Discount_Factor * Value_Function(S1));
-    --                 end loop;
-    --             end loop;
-    --             -- Get the arg max
-    --             Prev_Action := Policy(S);
-    --             Policy(S) := Arg_Max(Action_Values);
-    --             if Policy(S) /= Prev_Action then
-    --                 Stable := False;
-    --             end if;
-    --         end loop;
-    --         exit when Stable;
-    --     end loop;
-    --     return Policy;
-    -- end Policy_Iteration;
-   function Policy_Iteration(Model : DP_Model_Type; Discount_Factor : Float) return Deterministic_Policy_Type is
-      package Action_Random is new Ada.Numerics.Discrete_Random(Result_Subtype => Action_Type);
-      Action_Gen: Action_Random.Generator;
-       
+   function Policy_Iteration_With_Init(
+      Model : DP_Model_Type; Discount_Factor : Float;
+      Init_Value_Func : Value_Function_Type;
+      Init_Policy : Deterministic_Policy_Type
+   ) return Deterministic_Policy_Type is
       type Action_Value_Array_Type is array (Action_Type) of Float;
 
       function Arg_Max(Action_Values : Action_Value_Array_Type) return Action_Type is
@@ -143,17 +96,13 @@ package body RL.Algorithms.DP is
          return Best_Action;
       end Arg_Max;
 
-      Value_Function : Value_Function_Type := (others => 0.0);  -- TODO: Perhaps initialize to random values?
-      Policy : Deterministic_Policy_Type; -- := (others => Action_Random.Random(Action_Gen));  -- Initialize to random policy
+      Value_Function : Value_Function_Type := Init_Value_Func;
+      Policy : Deterministic_Policy_Type := Init_Policy;
 
       Stable : Boolean;
       Prev_Action : Action_Type;
       Action_Values : Action_Value_Array_Type;
    begin
-      Action_Random.Reset(Action_Gen);
-      Policy := (others => Action_Random.Random(Action_Gen));  -- Initialize to random policy
-      Print_Policy(Policy);  -- TODO: Remove after debugging
-
       loop 
          Value_Function := Iterative_Policy_Evaluation(Model, Policy, Discount_Factor, Value_Function);
     
@@ -176,5 +125,74 @@ package body RL.Algorithms.DP is
          exit when Stable;
       end loop;
       return Policy;
-    end Policy_Iteration;
+    end Policy_Iteration_With_Init;
+
+   function Policy_Iteration(Model : DP_Model_Type; Discount_Factor : Float) return Deterministic_Policy_Type is
+      package Action_Random is new Ada.Numerics.Discrete_Random(Result_Subtype => Action_Type);
+      Action_Gen: Action_Random.Generator;
+       
+      Init_Value_Function : Value_Function_Type := (others => 0.0);
+      Init_Policy : Deterministic_Policy_Type;
+
+   begin
+      Action_Random.Reset(Action_Gen);
+      -- Initialize to random policy
+      Init_Policy := (others => Action_Random.Random(Action_Gen));
+      Print_Policy(Init_Policy);  -- TODO: Remove after debugging
+      return Policy_Iteration_With_Init(Model, Discount_Factor, Init_Value_Function, Init_Policy);
+   end Policy_Iteration;
+
+   -- function Policy_Iteration(Model : DP_Model_Type; Discount_Factor : Float) return Deterministic_Policy_Type is
+   --    package Action_Random is new Ada.Numerics.Discrete_Random(Result_Subtype => Action_Type);
+   --    Action_Gen: Action_Random.Generator;
+   --     
+   --    type Action_Value_Array_Type is array (Action_Type) of Float;
+
+   --    function Arg_Max(Action_Values : Action_Value_Array_Type) return Action_Type is
+   --       Max_Value : Float := Float'First;
+   --       Best_Action : Action_Type := Action_Type'First;
+   --    begin
+   --       for A in Action_Type loop
+   --          if Action_Values(A) > Max_Value then
+   --             Max_Value := Action_Values(A);
+   --             Best_Action := A;
+   --          end if;
+   --       end loop;
+   --       return Best_Action;
+   --    end Arg_Max;
+
+   --    Value_Function : Value_Function_Type := (others => 0.0);  -- TODO: Perhaps initialize to random values?
+   --    Policy : Deterministic_Policy_Type; -- := (others => Action_Random.Random(Action_Gen));  -- Initialize to random policy
+
+   --    Stable : Boolean;
+   --    Prev_Action : Action_Type;
+   --    Action_Values : Action_Value_Array_Type;
+   -- begin
+   --    Action_Random.Reset(Action_Gen);
+   --    Policy := (others => Action_Random.Random(Action_Gen));  -- Initialize to random policy
+   --    Print_Policy(Policy);  -- TODO: Remove after debugging
+
+   --    loop 
+   --       Value_Function := Iterative_Policy_Evaluation(Model, Policy, Discount_Factor, Value_Function);
+   --  
+   --       Stable := True;
+   --       for S in State_Type loop
+   --          -- For state S, record the value of each action in Action_Values.
+   --          for A in Action_Type loop
+   --             Action_Values(A) := 0.0;
+   --             for S1 in State_Type loop
+   --                Action_Values(A) := Action_Values(A) + Model(S, A, S1).Probability * (Model(S, A, S1).Reward + Discount_Factor * Value_Function(S1));
+   --             end loop;
+   --          end loop;
+   --          -- Get the arg max
+   --          Prev_Action := Policy(S);
+   --          Policy(S) := Arg_Max(Action_Values);
+   --          if Policy(S) /= Prev_Action then
+   --             Stable := False;
+   --          end if;
+   --       end loop;
+   --       exit when Stable;
+   --    end loop;
+   --    return Policy;
+   --  end Policy_Iteration;
 end RL.Algorithms.DP;
