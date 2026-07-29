4eb711baa4a5c7b3d58e0b40b4666cf4a294f4bb
Justin Smith <jvstinian@gmail.com>
2026-07-29 10:07:22 -0400

    Adding test to check system properties.  Adding a test for navigating linewalk with random actions.

--- experiments/c/rl/tests/test_example.c
+++ experiments/c/rl/tests/test_example.c
@@ -1,6 +1,7 @@
 #include <check.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <reinforcementlearning/envs/linewalk.h>
 
 
 START_TEST (test_example1)
@@ -12,6 +13,18 @@ END_TEST
 START_TEST (test_example2)
 {
     ck_assert_msg(1, "Example failure!");
+}
+
+START_TEST (test_linewalk_random_actions)
+{
+    struct LineWalkConfig config = { 7 };
+    struct SimulationSummary sim_summary;
+    sim_summary = linewalk_uniform_random_actions(config, /* verbose */ FALSE);
+
+    ck_assert_msg(sim_summary.num_steps <= 10, "Steps exceeded 10");
+    ck_assert_msg(sim_summary.total_reward >= -1.0, "Reward is less than -1");
+    ck_assert_msg(sim_summary.total_reward <= 1.0, "Reward exceeds 1");
+    /* Seed_Reset_Type'(Kind => Set_Seed, Seed => 123) */
 }
 
 Suite * example_test_suite(void)
@@ -33,14 +46,31 @@ Suite * example_test_suite(void)
     return s;
 }
 
+Suite * rl_environments_test_suite(void)
+{
+    Suite *s;
+    TCase *tc_linewalk_random_actions;
+
+    s = suite_create("RL Environments");
+
+    tc_linewalk_random_actions = tcase_create("Linewalk Random Actions");
+
+    tcase_add_test(tc_linewalk_random_actions, test_linewalk_random_actions);
+    suite_add_tcase(s, tc_linewalk_random_actions);
+    
+    return s;
+}
+
 int main(void)
 {
     int number_failed;
-    Suite *s;
+    Suite *s, *rlenv_suite;
     SRunner *sr;
 
     s = example_test_suite();
+    rlenv_suite = rl_environments_test_suite();
     sr = srunner_create(s);
+    srunner_add_suite(sr, rlenv_suite);
 
     srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
