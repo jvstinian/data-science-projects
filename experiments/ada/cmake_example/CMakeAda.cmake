64721466f235b27255812fa339ca3295c777dfac
jvstinian <jvstinian@gmail.com>
2026-08-05 08:38:53 -0400

    RL C run options (#186)
    * Add support for specifying the environment and algorithm to use in the example application.
    * RL C preliminary cliffwalking environment (#187)
    * Adding a preliminary implementation of the cliffwalking environment.
    * Clean up Python bindings example for Ada library (#188)
    * Cleaning up Ada Python bindings example.
    * RL C cliffwalking environment improvements (#189)
    * Clean up of cliffwalking environment.
    * Removing the local copy of the cliffwalking map from the environment as only a single immutable map is used.
    * For the cliffwalking environment, adding a method for simulating use random actions and adding the DP methods using the template header.  Test cases similar to those used for frozenlake have also been added. (#190)

--- experiments/ada/cmake_example/CMakeAda.cmake
+++ experiments/ada/cmake_example/CMakeAda.cmake
@@ -1,9 +1,8 @@
 cmake_minimum_required(VERSION 3.20)
 
-# Builds the project and creates a lib variable to be used in linking named TARGETLib
-# LIBFILE must not have prefix nor suffix, and is expected to be static: libadatest.a -> adatest
+# Builds the project and creates a lib variable to be used in linking
+# This is for building a static library
 function(add_ada_library TARGET GPRFILE OUTPUT_REL_DIR)
-
     # Actual library file
     set(_result ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_REL_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${TARGET}${CMAKE_STATIC_LIBRARY_SUFFIX})
 
@@ -17,21 +16,18 @@ function(add_ada_library TARGET GPRFILE OUTPUT_REL_DIR)
     add_library(${TARGET} STATIC IMPORTED GLOBAL)
     add_dependencies(${TARGET} ${TARGET}_build)
 
-    # Connect the library object with the library target (that share the name!)
+    # Connect the library object with the library target
     set_target_properties(${TARGET}
             PROPERTIES
             IMPORTED_LOCATION ${_result})
-    #  INTERFACE_SOURCES ${CMAKE_SOURCE_DIR}/src
-
 endfunction(add_ada_library)
 
 function(add_ada_shared_library TARGET GPRFILE OUTPUT_REL_DIR)
-
     # Actual library file
     set(_result ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_REL_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}${TARGET}${CMAKE_SHARED_LIBRARY_SUFFIX})
 
     # Build and leave Ada dependencies to gprbuild
-    # BYPRODUCTS is included below to indicate to Ninja to use this target to build the shaared library
+    # BYPRODUCTS is included below to indicate to Ninja to use this target to build the shared library
     add_custom_target(${TARGET}_build
         COMMAND gprbuild -p -P${CMAKE_SOURCE_DIR}/${GPRFILE} --relocate-build-tree=${CMAKE_CURRENT_BINARY_DIR}
         BYPRODUCTS ${_result}
@@ -42,23 +38,19 @@ function(add_ada_shared_library TARGET GPRFILE OUTPUT_REL_DIR)
     add_library(${TARGET} SHARED IMPORTED GLOBAL)
     add_dependencies(${TARGET} ${TARGET}_build)
 
-    # Connect the library object with the library target (that share the name!)
+    # Connect the library object with the library target
     set_target_properties(${TARGET}
             PROPERTIES
             IMPORTED_LOCATION ${_result})
-    #  INTERFACE_SOURCES ${CMAKE_SOURCE_DIR}/src
-
 endfunction(add_ada_shared_library)
 
 function(add_ada_executable TARGET GPRFILE)
-
     # Actual binary file
     set(_result ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_REL_DIR}/${TARGET}${CMAKE_EXECUTABLE_SUFFIX})
 
     # Build and leave Ada dependencies to gprbuild
     add_custom_target(${TARGET}
-            ALL
-            COMMAND gprbuild -p -P${CMAKE_SOURCE_DIR}/${GPRFILE} --relocate-build-tree=${CMAKE_CURRENT_BINARY_DIR}
-            COMMENT "Building binary ${TARGET}...")
-
+        ALL
+        COMMAND gprbuild -p -P${CMAKE_SOURCE_DIR}/${GPRFILE} --relocate-build-tree=${CMAKE_CURRENT_BINARY_DIR}
+        COMMENT "Building binary ${TARGET}...")
 endfunction(add_ada_executable)
--- experiments/c/rl/apps/main.c
+++ experiments/c/rl/apps/main.c
@@ -1,21 +1,42 @@
+#define _GNU_SOURCE /* getopt */
+/* #define _POSIX_C_SOURCE 2 getopt */
 #include <stdio.h>
+#include <unistd.h>
+#include <ctype.h> /* tolower */
 #include "reinforcementlearning/envs/linewalk.h"
 #include "reinforcementlearning/envs/frozenlake.h"
 #include "reinforcementlearning/envs/carrental.h"
 #include "reinforcementlearning/algorithms/mcts_unbounded.h"
 
-enum Environments {
-    LINEWALK,
-    FROZENLAKE
+enum Environment {
+    ENV_LINEWALK,
+    ENV_FROZENLAKE,
+    ENV_CARRENTAL,
+    ENV_UNKNOWN
 };
 
-enum Algorithms {
-    RL_RANDOM_ACTIONS
+const char *env_names[4] = {
+    "linewalk",
+    "frozenlake",
+    "carrental",
+    "unknown"
+};
+
+enum Algorithm {
+    ALG_RL_RANDOM_ACTIONS,
+    ALG_DP_POLICY_ITERATION,
+    ALG_UNKNOWN
+};
+
+const char *alg_names[3] = {
+    "rl_random_actions",
+    "dp_policy_iteration",
+    "unknown"
 };
 
 struct RunConfig {
-    enum Environments env_name;
-    enum Algorithms algorithm_name;
+    enum Environment env;
+    enum Algorithm alg;
 };
 
 int linewalk_example() {
@@ -87,11 +108,137 @@ int frozenlake_mc_policy_evaluation_example() {
     return status;
 }
 
-int main() {
+int process_environment_value(char *argv, struct RunConfig *run_config) {
+    char env_value[16];
+    size_t i;
+    strncpy(env_value, argv, sizeof(env_value) - 1);
+    env_value[sizeof(env_value) - 1] = '\0'; /* Ensure null-termination */
+    for (i = 0; i < sizeof(env_value); i++) {
+        env_value[i] = (char) tolower((int) env_value[i]);
+    }
+    if (strcmp(env_value, "linewalk") == 0) {
+        run_config->env = ENV_LINEWALK;
+    } else if (strcmp(env_value, "frozenlake") == 0) {
+        run_config->env = ENV_FROZENLAKE;
+    } else if (strcmp(env_value, "carrental") == 0) {
+        run_config->env = ENV_CARRENTAL;
+    } else {
+        run_config->env = ENV_UNKNOWN;
+        fprintf(stderr, "Unknown environment: %s\n", argv);
+        return 1;
+    }
+    return 0;
+}
+
+int process_algorithm_value(char *argv, struct RunConfig *run_config) {
+    char alg_value[24];
+    size_t i;
+    strncpy(alg_value, argv, sizeof(alg_value) - 1);
+    alg_value[sizeof(alg_value) - 1] = '\0'; /* Ensure null-termination */
+    for (i = 0; i < sizeof(alg_value); i++) {
+        alg_value[i] = (char) tolower((int) alg_value[i]);
+    }
+    if (strcmp(alg_value, "rl_random_actions") == 0) {
+        run_config->alg = ALG_RL_RANDOM_ACTIONS;
+    } else if (strcmp(alg_value, "dp_policy_iteration") == 0) {
+        run_config->alg = ALG_DP_POLICY_ITERATION;
+    } else {
+        run_config->alg = ALG_UNKNOWN;
+        fprintf(stderr, "Unknown algorithm: %s\n", argv);
+        return 1;
+    }
+    return 0;
+}
+
+void print_help() {
+    printf("Usage: program [-h] [-e environment] [-a algorithm]\n");
+    printf("Options:\n");
+    printf("  -h               Show this help message\n");
+    printf("  -e environment   Specify the environment.  Possible values:\n");
+    printf("                     linewalk, frozenlake, carrental\n");
+    printf("  -a algorithm     Specify the algorithm.  Possible values:\n");
+    printf("                     rl_random_actions, dp_policy_iteration\n");
+}
+
+Boolean supported_env_alg[ENV_UNKNOWN][ALG_UNKNOWN] = {
+    {  TRUE, FALSE },  /* ENV_LINEWALK */
+    { FALSE,  TRUE },   /* ENV_FROZENLAKE */
+    { FALSE,  TRUE }   /* ENV_CARRENTAL */
+};
+
+int process_arguments(int argc, char *argv[], struct RunConfig *run_config) {
+    int opt;
+    int i;
+    (void)run_config; /* Suppress unused variable warning */
+    
+    while ((opt = getopt(argc, argv, "he:a:")) != -1) {
+        switch (opt) {
+            case 'h':
+                print_help();
+                exit(0);
+            case 'e':
+                /* printf("Environment specified: %s\n", optarg); */
+                if (process_environment_value(optarg, run_config)) {
+                    return 1; /* Error processing environment value */
+                }
+                break;
+            case 'a':
+                /* printf("Algorithms specified: %s\n", optarg); */
+                if (process_algorithm_value(optarg, run_config)) {
+                    return 1; /* Error processing algorithm value */
+                }
+                break;
+            case '?': /* Unknown option or missing required argument */
+                fprintf(stderr, "Unknown option or missing argument: -%c\n", optopt);
+                return 1;
+        }
+    }
+
+    if (optind < argc) {
+        fprintf(stderr, "Non-option arguments:\n");
+        for (i = optind; i < argc; i++) {
+            printf("  %s\n", argv[i]);
+        }
+        return 1;
+    }
+    
+    /* Additional validation */
+    if (run_config->env == ENV_UNKNOWN && run_config->alg == ALG_UNKNOWN) {
+        fprintf(stderr, "Environment and algorithm must be specified.\n");
+        print_help();
+        return 1;
+    } else if (run_config->env == ENV_UNKNOWN) {
+        fprintf(stderr, "The algorithm %s only supports the following environments:\n", alg_names[run_config->alg]);
+        for (i = 0; i < (int)ENV_UNKNOWN; i++) {
+            if (supported_env_alg[i][run_config->alg]) {
+                fprintf(stderr, "  %s\n", env_names[i]);
+            }
+        }
+        return 2;
+    } else if (run_config->alg == ALG_UNKNOWN) {
+        fprintf(stderr, "The environment %s only supports the following algorithms:\n", env_names[run_config->env]);
+        for (i = 0; i < (int)ALG_UNKNOWN; i++) {
+            if (supported_env_alg[run_config->env][i]) {
+                fprintf(stderr, "  %s\n", alg_names[i]);
+            }
+        }
+        return 2;
+    } else if (!supported_env_alg[run_config->env][run_config->alg]) {
+        fprintf(stderr, "The specified environment and algorithm combination is not supported.\n");
+        return 3;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    struct RunConfig run_config = { ENV_UNKNOWN, ALG_UNKNOWN };
     /*
     return linewalk_example();
     return frozenlake_mc_policy_evaluation_example();
     */
+    if (process_arguments(argc, argv, &run_config) != 0) {
+        return 1;
+    }
     carrental_dp_example();
     frozenlake_example_main();
     return uct_main();
