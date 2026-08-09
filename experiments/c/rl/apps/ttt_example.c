b16370ba022d718d36be91bcc09ae0d0e1cb19e4
jvstinian <jvstinian@gmail.com>
2026-08-09 09:10:30 -0400

    RL C MCTS for Hex environment (#200)
    * Prefixing names of method for the Hex environment.
    * Adding UCT methods from template header for the Hex environment.
    * The original sequential approach for finding winning paths in Hex was not correct.  We switch to a recursive approach here.  Code clean up is needed.
    * Adding comments explaining hexagon neighbors.
    * Commenting out the old methods for identifying a winning path in hex.

--- experiments/c/rl/apps/ttt_example.c
+++ experiments/c/rl/apps/ttt_example.c
@@ -6,8 +6,15 @@ int main() {
     return ttt_uct_example();
 }
 */
+/*
 #include <reinforcementlearning/envs/ataxx.h>
 
 int main() {
     return ataxx_uct_example();
+}
+*/
+#include <reinforcementlearning/envs/hex.h>
+
+int main() {
+    return hex_uct_example();
 }
